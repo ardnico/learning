f725ebaf124a1d75489195969f03101350470f16
LeafNico <leaf.sun2@gmail.com>
2025-09-28 21:42:30 +0900

    Step1_ad_3

--- Step1/src/hello_cli/src/main.rs
+++ Step1/src/hello_cli/src/main.rs
@@ -1,19 +1,30 @@
 use std::env;
 
 fn main() {
+    let mut shout_flag: i32 = 0;
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
         println!("名前を入力してください");
         return;
     }
+    if args.contains(&"--shout".to_string()){
+        shout_flag = 1;
+    }
 
     let mut names: String = "".to_string();
     for i in 1..args.len() {
+        if &args[i] == "--shout" {
+            continue;
+        }
         if i > 1{
             names += " and "; 
         }
-        names += &args[i];
+        if shout_flag == 1 {
+            names += &args[i].to_uppercase();
+        } else {
+            names += &args[i];
+        }
     }
     println!("Hello, {}!", names);
 }
